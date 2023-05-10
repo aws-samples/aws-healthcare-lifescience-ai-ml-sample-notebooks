30cd1d18962d823013aa01ee7d2cca3447c958f5
Brian <bloyal@amazon.com>
2023-05-10 19:04:25 +0000

    Completed module 2

---
+++ workshops/AI_Driven_Protein_Analysis/prothelpers/sequence.py
@@ -0,0 +1,24 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+from Bio.SeqIO.FastaIO import FastaIterator
+import os
+
+def list_files_in_dir(dir, extension = ".txt"):
+    paths = []
+    for filename in os.listdir(dir):
+        full_path = os.path.abspath(os.path.join(dir, filename))
+        if filename.endswith(extension):
+            paths.append(full_path)
+    paths.sort()
+    return(paths)
+
+def extract_seqs_from_dir(dir, extension = ".fa"):
+    
+    file_list = list_files_in_dir(dir, extension)
+    sequences = []
+    for file in file_list:
+        with open(file,"r") as f:
+            sequences.extend([str(record.seq) for record in FastaIterator(f)])
+    return(sequences)
+
--- workshops/AI_Driven_Protein_Analysis/prothelpers/structure.py
+++ workshops/AI_Driven_Protein_Analysis/prothelpers/structure.py
@@ -1,7 +1,14 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 from Bio import PDB
 from Bio.PDB.Polypeptide import three_to_one, is_aa
 import warnings
-
+from statistics import mean
+from math import ceil
+from prothelpers.sequence import list_files_in_dir
+import os
+import py3Dmol
 
 def atoms_to_pdb(input):
     """Convert Biopython structure to a PDB string"""
@@ -70,3 +77,44 @@ def get_aa_seq(input):
             if is_aa(res.resname)
         ]
     return "".join(residue_list)
+
+def get_average_plddt(input_file):
+    plddts = []    
+    with open(input_file, 'r') as in_file:
+        for line in in_file:
+            if line.startswith(('ATOM', 'HETATM')):
+                plddt = float(line[60:66].rstrip())
+                plddts.append(plddt)                
+    return mean(plddts)
+
+def create_tiled_py3dmol_view(structures, total_cols = 2, width = 500, height = 500):
+    total_rows = len(structures) // total_cols
+    view = py3Dmol.view(viewergrid=(total_rows, total_cols), width=width, height=height)
+    view.removeAllModels()
+    str = structures[0]
+    str_iter = iter(structures)
+    for i in range(total_rows):
+        for j in range(total_cols):
+            view.addModel(next(str_iter), "pdb", viewer=(i, j))     
+    return(view)
+
+def extract_structures_from_dir(dir, extension = ".pdb"):
+    
+    file_list = list_files_in_dir(dir, extension)
+    
+    structures = []
+    for obj in file_list:
+        if extension in obj:
+            p = os.path.join(dir, obj)
+            with open(p, "r") as f:
+                structures.append(f.read())
+    return(structures)
+
+def get_mean_plddt(structure):
+    plddts = []    
+    lines = structure.split("\n")
+    for line in lines:
+        if line.startswith(('ATOM', 'HETATM')):
+            plddt = float(line[60:66].rstrip())
+            plddts.append(plddt)                
+    return mean(plddts)
