7f7ecec22906e9aae81c428b618138fcff203eb1
Your Name <you@example.com>
2023-09-18 14:54:37 +0000

    Native pytorch Traning added

--- workshops/Protein_Language_Modelling/scripts/training/cuda/cuda-oas-mlm-train-smddp.py
+++ workshops/Protein_Language_Modelling/scripts/training/cuda/cuda-oas-mlm-train-smddp.py
@@ -31,7 +31,6 @@ from torch.optim import AdamW
 from torch.utils.data import DataLoader
 from torch.utils.data.distributed import DistributedSampler
 from tqdm.auto import tqdm
-import json
 from transformers import (
     AutoTokenizer,
     EsmForMaskedLM,
@@ -42,13 +41,25 @@ from transformers import (
 )
 from transformers.models.esm.configuration_esm import get_default_vocab_list
 
-### 0. Import Torch Distributed Training
-import torch.distributed as dist
+## SageMaker Distributed code.
+from smdistributed.dataparallel.torch.parallel.distributed import (
+    DistributedDataParallel as DDP,
+)
+import smdistributed.dataparallel.torch.distributed as dist
+
+dist.init_process_group()
 
 
 def parse_args():
     """Parse the arguments."""
     parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--eval_dir",
+        type=str,
+        default=os.environ["SM_CHANNEL_TEST"],
+        help="Path to evaluation dataset.",
+    )
     parser.add_argument(
         "--lr", type=float, default=5e-5, help="Learning rate to use for training."
     )
@@ -118,12 +129,6 @@ def parse_args():
         help="Path to train dataset.",
     )
     parser.add_argument(
-        "--test_dir",
-        type=str,
-        default=os.environ["SM_CHANNEL_TEST"],
-        help="Path to evaluation dataset.",
-    )
-    parser.add_argument(
         "--logging_steps",
         type=int,
         default=10,
@@ -153,12 +158,6 @@ def parse_args():
         default=0,
         help="Initialize random weights?",
     )
-
-    ########################## Step 1 : DDP related arguements ###########################
-    parser.add_argument('--hosts', type=list, default=json.loads(os.environ['SM_HOSTS']))
-    parser.add_argument('--current-host', type=str, default=os.environ['SM_CURRENT_HOST'])
-    parser.add_argument('--num-gpus', type=int, default=os.environ['SM_NUM_GPUS'])
-    ########################## DDP related ###########################    
 
     args, _ = parser.parse_known_args()
     return args
@@ -191,127 +190,20 @@ def report_metrics(
     return None
 
 
-def worker(local_rank, args):
-    
+def main(args):
     run_start = timer()
-
-    # Step 4: Compute the global rank (global_rank) of the spawned process as:
-    # =node_id*num_gpus + local_rank.
-    # To properly initialize and synchornize each process, 
-    # invoke dist.init_process_group with the approrpriate parameters:
-    # backend='nccl', world_size=WORLD_SIZE, rank=global_rank
-
-    world_size = len(args.hosts) * args.num_gpus
-    
-    #ToDO : check below
-    os.environ['WORLD_SIZE'] = str(world_size)
-    print("[DDP] World Size is [{}]".format(world_size))
-
-    node_id = args.hosts.index(args.current_host)
-    global_rank = node_id * args.num_gpus + local_rank
-    os.environ['RANK'] = str(global_rank)
-    
-    dist.init_process_group(backend="nccl", world_size=world_size, rank=global_rank)
-
-    print("[DDP] Initialized the distributed environment: {} backend on {} nodes. ".format(
-            'nccl', dist.get_world_size()) + 'Current host rank is {}. Number of gpus: {}'.format(
-            dist.get_rank(), args.num_gpus))   
-
-
     if args.seed is not None:
         set_seed(args.seed)
         torch.manual_seed(args.seed)
         torch.cuda.manual_seed(args.seed)
 
+    device = torch.device("cuda")
 
-    # DDP Step 5: Download the data only once per host, however then running on SageMaker data already downloaded
+    world_size = dist.get_world_size()
+    rank = dist.get_rank()
+    local_rank = dist.get_local_rank()
 
-    # data_src = "bloyal/oas_paired_human_sars_cov_2"
-    # train_sample_count = args.train_sample_count
-    # test_sample_count = int(train_sample_count * 0.2)
-
-    # if local_rank == 0:
-    #     print("[DDP] Started Downloading data in host [{}] rank [{}]".format(args.current_host, local_rank))
-    #     train_dataset = load_dataset(path=args.training_dir, split=f"train[:{train_sample_count}]", download_mode="force_redownload")
-    #     test_dataset = load_dataset(path=args.test_dir, split=f"test[:{test_sample_count}]", download_mode="force_redownload")
-    #     print("[DDP] Finished Downloading data in host [{}] rank [{}]".format(args.current_host, local_rank))
-    # dist.barrier()
-    
-    # if local_rank != 0:
-    #     print("[DDP] Reusing Downloaded data in host [{}] rank [{}]".format(args.current_host, local_rank))
-    #     train_dataset = load_dataset(data_src, split=f"train[:{train_sample_count}]", download_mode="reuse_dataset_if_exists")
-    #     test_dataset = load_dataset(data_src, split=f"test[:{test_sample_count}]", download_mode="reuse_dataset_if_exists")
-    #     print("[DDP] Reused Downloaded data in host [{}] rank [{}]".format(args.current_host, local_rank))
-
-    train_dataset = load_from_disk(args.training_dir)
-    test_dataset = load_from_disk(args.test_dir)
-    
-    # DDP Step 6: Wrap training and validation data with DistributedSampler, and enable data shuffling
-    train_sampler = torch.utils.data.distributed.DistributedSampler(
-        train_dataset,
-        num_replicas=world_size,
-        rank=global_rank,
-        shuffle=True
-    )
-    
-    test_sampler = torch.utils.data.distributed.DistributedSampler(
-        test_dataset,
-        num_replicas=world_size,
-        rank=global_rank,
-        shuffle=True
-    )
-
-    print("[DDP] Train and test samplers set in host [{}] rank [{}]".format(args.current_host, local_rank))
-
-    tokenizer = AutoTokenizer.from_pretrained(args.model_id)
-    tokenizer.model_max_length = args.max_length
-    data_collator = DataCollatorForLanguageModeling(
-        tokenizer=tokenizer, mlm_probability=0.15
-    )
-    train_loader = DataLoader(
-        train_dataset,
-        collate_fn=data_collator,
-        batch_size=args.per_device_train_batch_size,
-        sampler=train_sampler,
-        shuffle=False if train_sampler else True,
-    )
-    eval_loader = DataLoader(
-        test_dataset,
-        collate_fn=data_collator,
-        batch_size=args.per_device_eval_batch_size,
-        sampler=test_sampler,
-        shuffle=False if test_sampler else True,
-    )
-
-
-
-    # 
-    # test_sample_count = int(train_sample_count * 0.2)
-    # train_dataset = load_dataset(src, split=f"train[:{train_sample_count}]")
-    # test_dataset = load_dataset(src, split=f"test[:{test_sample_count}]")
-    # dataset = DatasetDict({"train": train_dataset, "test": test_dataset}).rename_column(
-    #     "sequence_alignment_aa_heavy", "text"
-    # )
-    # tokenizer = AutoTokenizer.from_pretrained("facebook/esm2_t6_8M_UR50D")
-    # sequence_length = 142
-    # encoded_dataset = dataset.map(
-    #     tokenize_data,
-    #     batched=True,
-    #     num_proc=os.cpu_count(),
-    #     remove_columns=dataset["train"].column_names,
-    #     fn_kwargs={
-    #         "tokenizer": tokenizer,
-    #         "sequence_length": sequence_length,
-    #     },
-    # )
-
-    # encoded_dataset.set_format("torch", columns=["input_ids", "attention_mask"])
-    # return encoded_dataset
-
-    # DDP Step 7: Modify the torch.device call from "cuda:0" to "cuda:<local_rank>" 
-    # to pin the process to its assigned GPU. 
-    device = torch.device("cuda:" + str(local_rank) if torch.cuda.is_available() else "cpu")
-    print("[DDP] Device identified in host [{}] rank [{}] as [{}]".format(args.current_host, local_rank, device))
+    print(f"Start running DDP on rank {rank}.")
 
     ## Load model
     model = EsmForMaskedLM.from_pretrained(args.model_id)
@@ -320,10 +212,65 @@ def worker(local_rank, args):
         my_config.vocab_list = get_default_vocab_list()
         my_config.vocab_size = len(my_config.vocab_list)
         model = EsmForMaskedLM(my_config)
-
     model.to(device)
-    model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[local_rank], find_unused_parameters=True)
-    print("[DDP] Model loaded in host [{}] rank [{}]".format(args.current_host, local_rank))
+    model = DDP(model, find_unused_parameters=True)
+    torch.cuda.set_device(local_rank)
+    model.cuda(local_rank)
+
+    is_root = rank == 0
+
+    if args.train_sample_count is not None:
+        if rank > 0:
+            torch.distributed.barrier()
+        dataset = get_data(args.train_sample_count)
+        train_dataset = dataset["train"]
+        eval_dataset = dataset["test"]
+        if rank == 0:
+            torch.distributed.barrier()
+    else:
+        train_dataset = load_from_disk(args.training_dir)
+        eval_dataset = load_from_disk(args.eval_dir)
+
+    if is_root:
+        print(f"Loaded train_dataset length is: {len(train_dataset)}")
+        print(f"Loaded test_dataset length is: {len(eval_dataset)}")
+
+    tokenizer = AutoTokenizer.from_pretrained(args.model_id)
+    tokenizer.model_max_length = args.max_length
+    data_collator = DataCollatorForLanguageModeling(
+        tokenizer=tokenizer, mlm_probability=0.15
+    )
+
+    train_sampler = None
+    eval_sampler = None
+    if world_size > 1:  # if more than one core
+        train_sampler = DistributedSampler(
+            train_dataset,
+            num_replicas=world_size,
+            rank=rank,
+            shuffle=True,
+        )
+        eval_sampler = DistributedSampler(
+            eval_dataset,
+            num_replicas=world_size,
+            rank=rank,
+            shuffle=True,
+        )
+
+    train_loader = DataLoader(
+        train_dataset,
+        collate_fn=data_collator,
+        batch_size=args.per_device_train_batch_size,
+        sampler=train_sampler,
+        shuffle=False if train_sampler else True,
+    )
+    eval_loader = DataLoader(
+        eval_dataset,
+        collate_fn=data_collator,
+        batch_size=args.per_device_eval_batch_size,
+        sampler=eval_sampler,
+        shuffle=False if eval_sampler else True,
+    )
 
     # Define training metrics
     num_update_steps_per_epoch = len(train_loader)
@@ -340,8 +287,16 @@ def worker(local_rank, args):
     samples_processed_per_eval = total_eval_batch_size * num_eval_steps_per_epoch
     tokens_processed_per_eval = samples_processed_per_eval * args.max_length
 
-    optimizer = AdamW(model.parameters(), args.lr)
+    ## Load model
+    model = EsmForMaskedLM.from_pretrained(args.model_id)
+    if args.pretrain:
+        my_config = copy.deepcopy(model.config)
+        my_config.vocab_list = get_default_vocab_list()
+        my_config.vocab_size = len(my_config.vocab_list)
+        model = EsmForMaskedLM(my_config)
 
+    model.to(device)
+    optimizer = AdamW(model.parameters(), args.lr)
     lr_scheduler = get_scheduler(
         name=args.lr_scheduler_type,
         optimizer=optimizer,
@@ -349,8 +304,7 @@ def worker(local_rank, args):
         num_training_steps=num_total_training_steps,
     )
 
-
-    if global_rank == 0:
+    if is_root:
         print("***** Running training *****")
         print(f"\nNum examples: {len(train_dataset)}")
         print(f"\nNum Epochs: {args.num_epochs}")
@@ -364,18 +318,15 @@ def worker(local_rank, args):
 
     # Only show the progress bar once on each machine.
     progress_bar = tqdm(
-        range(num_total_training_steps), disable=not local_rank == 0, miniters=1
+        range(num_total_training_steps), disable=not is_root, miniters=1
     )
     completed_steps = 0
     starting_epoch = 0
 
     # Start training loop
     for epoch in range(starting_epoch, args.num_epochs):
-        if local_rank == 0:
+        if is_root:
             print("######################### Train #########################")
-
-        train_sampler.set_epoch(epoch)
-
         model.train()
         for idx, batch in enumerate(train_loader):
             train_loop_start_time = timer()
@@ -397,7 +348,7 @@ def worker(local_rank, args):
             completed_steps += 1
             if (idx + 1) % args.logging_steps == 0:
                 report_metrics(
-                    local_rank,
+                    rank,
                     train_loop_start_time,
                     loss,
                     epoch,
@@ -407,12 +358,9 @@ def worker(local_rank, args):
                     "Training",
                 )
 
-        dist.barrier()
-
-        if local_rank==0:
+        if is_root:
             print("######################### Eval #########################")
         eval_start_time = timer()
-        
         model.eval()
         eval_running_loss = 0
         for batch in eval_loader:
@@ -423,7 +371,7 @@ def worker(local_rank, args):
             eval_running_loss += eval_loss.detach().float() / num_eval_steps_per_epoch
 
         report_metrics(
-            local_rank,
+            rank,
             eval_start_time,
             eval_running_loss,
             epoch,
@@ -434,7 +382,7 @@ def worker(local_rank, args):
         )
 
     # Save checkpoint for evaluation (xm.save ensures only one process save)
-    if global_rank == 0:
+    if is_root:
         model = model.module if hasattr(model, "module") else model
         os.makedirs(args.model_dir, exist_ok=True)
         checkpoint = {"state_dict": model.state_dict()}
@@ -482,13 +430,4 @@ def get_data(train_sample_count):
 
 if __name__ == "__main__":
     args = parse_args()
-
-    ############# Step 2: Compute world size (WORLD_SIZE) using num_gpus and num_nodes
-    # and specify the IP address/port number for the node associated with the main process (global rank = 0):
-    master = json.loads(os.environ['SM_TRAINING_ENV'])['master_hostname']
-
-    print("[DDP] master address is [{}]".format(master))
-    os.environ['MASTER_ADDR'] = master
-    os.environ['MASTER_PORT'] = '7777' 
-    #####################################################
-    torch.multiprocessing.spawn(worker, nprocs=args.num_gpus, args=(args,))
+    main(args)
