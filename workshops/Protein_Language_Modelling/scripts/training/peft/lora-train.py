1236b89bfa11a04628a9bb31d40ea5014a116914
Brian Loyal <bloyal@amazon.com>
2023-10-30 22:46:24 -0500

    Add ESM DeepLoc fine tuning

--- workshops/Protein_Language_Modelling/scripts/training/peft/lora-train.py
+++ workshops/Protein_Language_Modelling/scripts/training/peft/lora-train.py
@@ -1,20 +1,11 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
 
-import os
 import argparse
-from transformers import (
-    AutoModelForSequenceClassification,
-    AutoTokenizer,
-    set_seed,
-    DataCollatorWithPadding,
-    Trainer,
-    TrainingArguments,
-    EvalPrediction
-)
 from datasets import load_from_disk
-import torch
-import shutil
+import evaluate
+import numpy as np
+import os
 from peft import (
     get_peft_model,
     LoraConfig,
@@ -22,11 +13,121 @@ from peft import (
     prepare_model_for_kbit_training,
     PeftModel,
     PeftConfig,
+    PeftModelForSequenceClassification
 )
-from sklearn.metrics import f1_score, roc_auc_score, accuracy_score
-from transformers import EvalPrediction
-import numpy as np
+import pynvml
+import shutil
 import tempfile
+import torch
+from torchinfo import summary
+from transformers import (
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+    set_seed,
+    DataCollatorWithPadding,
+    EsmForSequenceClassification,
+    Trainer,
+    TrainingArguments,
+)
+
+
+def main(args):
+    set_seed(args.seed)
+
+    model = get_model(
+        args.model_id,
+        quantization=args.quantization,
+        lora=args.lora,
+        use_gradient_checkpointing=args.use_gradient_checkpointing,
+        mixed_precision=args.mixed_precision,
+    )
+
+    tokenizer = AutoTokenizer.from_pretrained(args.model_id)
+
+    data_collator = DataCollatorWithPadding(
+        tokenizer=tokenizer  # , padding="max_length", max_length=args.max_length
+    )
+
+    logging_steps = 16
+
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        training_args = TrainingArguments(
+            output_dir=tmp_dir,
+            overwrite_output_dir=True,
+            per_device_train_batch_size=args.per_device_train_batch_size,
+            per_device_eval_batch_size=args.per_device_eval_batch_size,
+            bf16=True if args.mixed_precision == "bf16" else None,
+            learning_rate=args.lr,
+            num_train_epochs=args.epochs,
+            gradient_accumulation_steps=args.gradient_accumulation_steps,
+            gradient_checkpointing=args.use_gradient_checkpointing,
+            logging_dir=f"{tmp_dir}/logs",
+            logging_strategy="steps",
+            logging_steps=logging_steps,
+            evaluation_strategy="epoch",
+            save_strategy="no",
+            optim=args.optim,
+            push_to_hub=False,
+        )
+
+        train_dataset = (load_from_disk(args.train_dataset_path),)
+        print(train_dataset)
+
+        # Create Trainer instance
+        trainer = Trainer(
+            model=model,
+            args=training_args,
+            train_dataset=load_from_disk(args.train_dataset_path),
+            eval_dataset=load_from_disk(args.eval_dataset_path),
+            data_collator=data_collator,
+            compute_metrics=compute_metrics,
+        )
+
+        # Start training
+        trainer.train()
+
+        gpu_memory_use = get_gpu_utilization()
+        print(f"Max GPU memory use during training: {gpu_memory_use} MB")
+
+        if args.lora:
+            trainer.model.save_pretrained(tmp_dir)
+            # clear memory
+            del model
+            del trainer
+            torch.cuda.empty_cache()
+            # load PEFT model in fp16
+            peft_config = PeftConfig.from_pretrained(tmp_dir)
+            model = AutoModelForSequenceClassification.from_pretrained(
+                peft_config.base_model_name_or_path,
+                return_dict=True,
+                torch_dtype=torch.float16,
+                low_cpu_mem_usage=True,
+                problem_type="single_label_classification",
+            )
+            model = PeftModel.from_pretrained(model, tmp_dir)
+
+            model.eval()
+            # Merge LoRA and base model and save
+            merged_model = model.merge_and_unload()
+            merged_model.save_pretrained(args.model_output_path)
+        else:
+            trainer.model.save_pretrained(args.model_output_path)
+
+    # save tokenizer for easy inference
+    tokenizer.save_pretrained(args.model_output_path)
+
+    # copy inference script
+    os.makedirs("/opt/ml/model/code", exist_ok=True)
+    shutil.copyfile(
+        os.path.join(os.path.dirname(__file__), "inference.py"),
+        "/opt/ml/model/code/inference.py",
+    )
+    shutil.copyfile(
+        os.path.join(os.path.dirname(__file__), "requirements.txt"),
+        "/opt/ml/model/code/requirements.txt",
+    )
+
 
 def parse_args():
     """Parse the arguments."""
@@ -61,13 +162,13 @@ def parse_args():
     parser.add_argument(
         "--per_device_train_batch_size",
         type=int,
-        default=2,
+        default=8,
         help="Batch size to use for training.",
     )
     parser.add_argument(
         "--per_device_eval_batch_size",
         type=int,
-        default=32,
+        default=16,
         help="Batch size to use for evaluation.",
     )
     parser.add_argument(
@@ -93,162 +194,117 @@ def parse_args():
         help="Max length of sequence for collator.",
     )
     parser.add_argument(
-        "--bf16",
-        type=bool,
-        default=True if torch.cuda.get_device_capability()[0] >= 8 else False,
-        help="Whether to use bf16.",
+        "--quantization",
+        type=str,
+        default=None,
+        help="Degree of bitsandbytes quantization",
+        choices=[
+            "8bit",
+            "4bit",
+        ],
     )
     parser.add_argument(
-        "--problem_type",
+        "--lora",
+        type=bool,
+        default=False,
+        help="Whether or not to train with low-ranked adaptors via PEFT",
+    )
+    parser.add_argument(
+        "--use_gradient_checkpointing",
+        type=bool,
+        default=False,
+        help="Whether or not to train with gradient checkpointing via PEFT",
+    )
+    parser.add_argument(
+        "--mixed_precision",
         type=str,
-        default="multi_label_classification",
-        help="Sequence classification type",
+        default="bf16" if torch.cuda.get_device_capability()[0] >= 8 else "fp16",
+        help="Whether or not to use mixed precision training. Choose from ‘no’,‘fp16’,‘bf16 or ‘fp8’",
     )
 
-    parser.add_argument("--num_labels", type=int, default=10, help="Number of classes")
     args = parser.parse_known_args()
     return args
 
 
-def create_peft_config(model, use_gradient_checkpointing=False, load_in_8bit=True):
+# Setup evaluation
+metric = evaluate.load("accuracy")
 
-    peft_config = LoraConfig(
-        task_type=TaskType.SEQ_CLS,
-        inference_mode=False,
-        r=8,
-        lora_alpha=32,
-        lora_dropout=0.05,
-        target_modules=["query", "value"],
-    )
 
-    if load_in_8bit:
-        # prepare int-8 model for training
-        model = prepare_model_for_kbit_training(
-            model, use_gradient_checkpointing=use_gradient_checkpointing
+def compute_metrics(eval_pred):
+    logits, labels = eval_pred
+    predictions = np.argmax(logits, axis=-1)
+    output = metric.compute(predictions=predictions, references=labels)
+    return output
+
+
+def get_quant_config(quantization=None):
+    if quantization == "4bit":
+        return BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_use_double_quant=True,
         )
-    model = get_peft_model(model, peft_config)
-    model.print_trainable_parameters()
+    elif quantization == "8bit":
+        return BitsAndBytesConfig(
+            load_in_8bit=True,
+        )
+    else:
+        return None
+
+
+def get_model(
+    model_name_or_path,
+    config=None,
+    trust_remote_code=False,
+    quantization=False,
+    lora=False,
+    use_gradient_checkpointing=False,
+    mixed_precision=None,
+):
+    id2label = {0: "Cytosolic", 1: "Membrane"}
+    label2id = {"Cytosolic": 0, "Membrane": 1}
+
+    model = EsmForSequenceClassification.from_pretrained(
+        model_name_or_path,
+        from_tf=bool(".ckpt" in model_name_or_path),
+        config=config,
+        trust_remote_code=trust_remote_code,
+        quantization_config=get_quant_config(quantization),
+        torch_dtype=torch.bfloat16 if mixed_precision == "bf16" else torch.float16,
+        num_labels=2,
+        id2label=id2label,
+        label2id=label2id,
+    )
+    print(summary(model))
+
+    if lora:
+        peft_config = LoraConfig(
+            task_type=TaskType.SEQ_CLS,
+            inference_mode=False,
+            r=8,
+            lora_alpha=32,
+            lora_dropout=0.05,
+            target_modules=["query", "key", "value"],
+        )
+
+        if quantization:      
+            model = prepare_model_for_kbit_training(model, use_gradient_checkpointing)
+
+        model = get_peft_model(model, peft_config)
+
+    print(summary(model))
+    print(model)
+
     return model
 
 
-# source: https://jesusleal.io/2021/04/21/Longformer-multilabel-classification/
-def multi_label_metrics(predictions, labels, threshold=0.5):
-    sigmoid = torch.nn.Sigmoid()
-    probs = sigmoid(torch.Tensor(predictions))
-    y_pred = np.zeros(probs.shape)
-    y_pred[np.where(probs >= threshold)] = 1
-    y_true = labels
-    f1_micro_average = f1_score(y_true=y_true, y_pred=y_pred, average="micro")
-    roc_auc = roc_auc_score(y_true, y_pred, average="micro")
-    accuracy = accuracy_score(y_true, y_pred)
-    metrics = {"f1": f1_micro_average, "roc_auc": roc_auc, "accuracy": accuracy}
-    return metrics
+def get_gpu_utilization():
+    pynvml.nvmlInit()
+    handle = pynvml.nvmlDeviceGetHandleByIndex(0)
+    info = pynvml.nvmlDeviceGetMemoryInfo(handle)
 
-
-def compute_metrics(p: EvalPrediction):
-    preds = p.predictions[0] if isinstance(p.predictions, tuple) else p.predictions
-    result = multi_label_metrics(predictions=preds, labels=p.label_ids)
-    return result
-
-
-def training_function(args):
-    set_seed(args.seed)
-
-    model = AutoModelForSequenceClassification.from_pretrained(
-        args.model_id,
-        load_in_8bit=True,
-        num_labels=args.num_labels,
-        problem_type=args.problem_type,
-        use_cache=True,
-    )
-
-    model = create_peft_config(
-        model, use_gradient_checkpointing=False, load_in_8bit=True
-    )
-
-    tokenizer = AutoTokenizer.from_pretrained(args.model_id)
-
-    data_collator = DataCollatorWithPadding(
-        tokenizer=tokenizer, padding="max_length", max_length=args.max_length
-    )
-
-    logging_steps = 16
-
-    with tempfile.TemporaryDirectory() as tmp_dir:
-
-        training_args = TrainingArguments(
-            output_dir=tmp_dir,
-            overwrite_output_dir=True,
-            per_device_train_batch_size=args.per_device_train_batch_size,
-            per_device_eval_batch_size=args.per_device_eval_batch_size,
-            bf16=args.bf16,
-            learning_rate=args.lr,
-            num_train_epochs=args.epochs,
-            gradient_accumulation_steps=args.gradient_accumulation_steps,
-            logging_dir=f"{tmp_dir}/logs",
-            logging_strategy="steps",
-            logging_steps=logging_steps,
-            evaluation_strategy="epoch",
-            save_strategy="no",
-            optim=args.optim,
-            push_to_hub=False,
-        )
-
-        # Create Trainer instance
-        trainer = Trainer(
-            model=model,
-            args=training_args,
-            train_dataset=load_from_disk(args.train_dataset_path),
-            eval_dataset=load_from_disk(args.eval_dataset_path),
-            data_collator=data_collator,
-            compute_metrics=compute_metrics,
-        )
-
-        # Start training
-        trainer.train()
-
-        
-        trainer.model.save_pretrained(tmp_dir)
-        # clear memory
-        del model
-        del trainer
-        torch.cuda.empty_cache()
-        # load PEFT model in fp16
-        peft_config = PeftConfig.from_pretrained(tmp_dir)
-        model = AutoModelForSequenceClassification.from_pretrained(
-            peft_config.base_model_name_or_path,
-            return_dict=True,
-            torch_dtype=torch.float16,
-            low_cpu_mem_usage=True,
-            num_labels=args.num_labels,
-            problem_type=args.problem_type,
-        )
-        model = PeftModel.from_pretrained(model, tmp_dir)
-
-    model.eval()
-    # Merge LoRA and base model and save
-    merged_model = model.merge_and_unload()
-    merged_model.save_pretrained(args.model_output_path)
-
-    # save tokenizer for easy inference
-    tokenizer.save_pretrained(args.model_output_path)
-
-    # copy inference script
-    os.makedirs("/opt/ml/model/code", exist_ok=True)
-    shutil.copyfile(
-        os.path.join(os.path.dirname(__file__), "inference.py"),
-        "/opt/ml/model/code/inference.py",
-    )
-    shutil.copyfile(
-        os.path.join(os.path.dirname(__file__), "requirements.txt"),
-        "/opt/ml/model/code/requirements.txt",
-    )
-
-
-def main():
-    args, _ = parse_args()
-    training_function(args)
+    return info.used // 1024**2
 
 
 if __name__ == "__main__":
-    main()
+    args, _ = parse_args()
+    main(args)
